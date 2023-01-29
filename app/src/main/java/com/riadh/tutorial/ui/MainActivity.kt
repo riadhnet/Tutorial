fe227dbb61c5557316ab0d22e4f2fafe6832af7d
Riadh <riadhmezzi@tabesto.com>
2023-01-29 17:12:04 +0100

    feat: show app version

--- app/src/main/java/com/riadh/tutorial/ui/MainActivity.kt
+++ app/src/main/java/com/riadh/tutorial/ui/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.riadh.tutorial.ui
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,7 +18,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainLayout(viewModel)
+            MainLayout(viewModel, getAppVersion())
         }
     }
+
+    private fun Context.getAppVersion(): String {
+        return try {
+            val packageInfo = packageManager.getPackageInfoCompat(packageName)
+            packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            "Unknown"
+        }
+    }
+
+    fun PackageManager.getPackageInfoCompat(packageName: String, flags: Int = 0): PackageInfo =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getPackageInfo(packageName, PackageManager.PackageInfoFlags.of(flags.toLong()))
+        } else {
+            @Suppress("DEPRECATION") getPackageInfo(packageName, flags)
+        }
 }
+
--- app/src/main/java/com/riadh/tutorial/ui/MainLayout.kt
+++ app/src/main/java/com/riadh/tutorial/ui/MainLayout.kt
@@ -1,31 +1,44 @@
 package com.riadh.tutorial.ui
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun MainLayout(viewModel: MainViewModel) {
+fun MainLayout(viewModel: MainViewModel, appVersion: String) {
     MaterialTheme {
         Surface(color = Color.White) {
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
                 viewModel.text.value?.let {
                     Text(
                         text = it,
                         modifier = Modifier.wrapContentSize()
                     )
                 }
+                Spacer(modifier = Modifier.weight(1f))
+                val textStyle = MaterialTheme.typography.body2
+
+                Text(
+                    text = "Version: $appVersion",
+                    style = textStyle,
+                    modifier = Modifier.wrapContentSize()
+                )
+
+
             }
         }
     }
@@ -34,5 +47,5 @@ fun MainLayout(viewModel: MainViewModel) {
 @Preview
 @Composable
 fun MainLayoutPreview() {
-    MainLayout(MainViewModel())
+    MainLayout(MainViewModel() , "1.4")
 }
